47b66c939b30578d8931b9570e97be22693ac4ad
Alexandre Terrasa <alexandre@moz-code.org>
2014-11-25 12:32:57 -0500

    add auto populate db
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./db');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+var populateDB = function() {
+	var messages = [
+        {
+			'title': 'Hello 5chanz!',
+			'mail': 'josh@bytheway.com',
+			'date': new Date(),
+			'anonymous': true,
+            'body': "Hello World!"
+        },
+		{
+			'title': 'STUFU noob!',
+			'mail': 'lolZeKat@kikoolol.ca',
+			'date': new Date(),
+			'anonymous': false,
+            'body': "Hello anon!"
+        }
+	];
+	db.getConnection(function(err, db) {
+        db.collection("messages", function(err, collection) {
+            collection.find().toArray(function(err, messages_coll) {
+                if(messages_coll.length == 0) {
+                    collection.insert(messages, {safe:true}, function(err, result) {});
+                }
+            });
+        });
+    });
+}
+
+populateDB();
 
 module.exports = router;
